f24581219e37da2dbd34e587072064ef2e028c70
Luísa Barros <qlullyq@gmail.com>
2019-04-25 21:35:39 -0300

    Init project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true,
+});
+
+app.set('view engine', 'njk');
+
+app.get('/', (req, res) => res.render('form'));
+
+app.listen(3000);
