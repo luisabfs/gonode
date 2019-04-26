6943edef05f5502beefe843c1d44c35a8281e52a
Luísa Barros <qlullyq@gmail.com>
2019-04-26 18:02:40 -0300

    Add check and page views

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 
+// express config
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 
+// nunjucks config
 nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true,
 });
-
 app.set('view engine', 'njk');
 
+const checkUser = (age, res) => {
+  if (age < 18) res.redirect(`/minor?age=${age}`);
+  else res.redirect(`/major?age=${age}`);
+};
+
 app.get('/', (req, res) => res.render('form'));
+
+app.post('/check', (req, res) => {
+  checkUser(req.body.age, res);
+});
+
+app.get('/minor', (req, res) => {
+  const minor = 'menor';
+  const major = 'maior';
+  console.log(req.body.age);
+  return req.body.age < 18 ? res.render('page', { minor }) : res.render('page', { major });
+});
 
 app.listen(3000);
